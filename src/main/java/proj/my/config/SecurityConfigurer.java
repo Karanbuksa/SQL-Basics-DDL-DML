34f6327670a3dba86fb522d2057559dac5d05b64
Artyom Bekkuzhinov <bekkygi27@gmail.com>
2023-08-30 16:53:14 +0500

    secured methods, old way

--- src/main/java/proj/my/config/SecurityConfigurer.java
+++ src/main/java/proj/my/config/SecurityConfigurer.java
@@ -1,25 +1,33 @@
 package proj.my.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("Ivan").password("{noop}1234").roles("USER");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().and()
-                .authorizeRequests().antMatchers("/persons/by-city").permitAll()
+        auth.inMemoryAuthentication()
+                .withUser("Ivan").password(encoder().encode("password"))
+                .roles("READ", "WRITE")
                 .and()
-                .authorizeRequests().anyRequest().authenticated();
-
-
+                .withUser("Vasiliy").password(encoder().encode("password2"))
+                .roles("READ")
+                .and()
+                .withUser("Tamara").password(encoder().encode("password3"))
+                .roles("DELETE");
     }
+
+
 }
